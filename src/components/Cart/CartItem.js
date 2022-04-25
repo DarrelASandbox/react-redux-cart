57657a15bff7bf08c7b7d4d5060f333473a01080
DarrelA <darrelaiscoding@gmail.com>
2022-04-25 11:23:51 +0800

    Connecting cart-slice & ui-slice

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,23 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import classes from './CartItem.module.css';
 
-const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+const CartItem = ({ item }) => {
+  const { id, title, quantity, totalPrice, price } = item;
+
+  const dispatch = useDispatch();
+
+  const removeCartItemHandler = () => dispatch(cartActions.removeCartItem(id));
+
+  const addCartItemHandler = () =>
+    dispatch(cartActions.addCartItem({ id, title, price }));
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${totalPrice.toFixed(2)}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +26,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeCartItemHandler}>-</button>
+          <button onClick={addCartItemHandler}>+</button>
         </div>
       </div>
     </li>
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,12 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
-const ProductItem = (props) => {
-  const { title, price, description } = props;
+const ProductItem = ({ id, title, price, description }) => {
+  const dispatch = useDispatch();
+  const addCartItem = () =>
+    dispatch(cartActions.addCartItem({ id, title, price }));
 
   return (
     <li className={classes.item}>
@@ -13,7 +17,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addCartItem}>Add to Cart</button>
         </div>
       </Card>
     </li>
